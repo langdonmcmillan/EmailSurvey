ca8d3441239dc8785ef45322517cc620e8c4b5db
Langdon McMillan <langdon.mcmillan@gmail.com>
2017-09-03 13:25:04 -0400

    adding google oauth

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 const express = require("express");
+require("../services/passport");
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send({
-        hi: "there"
-    });
-})
+require("./routes/auth-routes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
